697ed9c7a6bf0f977d9c131da2c3e7955d1efe7c
Sujan S <sujanshivananda5@gmail.com>
2025-04-18 18:12:51 +0530

    Refactor routing in App.jsx to include About component and update Home1 path

--- hackfest/src/App.jsx
+++ hackfest/src/App.jsx
@@ -4,6 +4,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css'
 import Navbar from './components/Navbar'
 import Home1 from './components/Home1'
+import About from './components/About'
 
 function App() {
   const router = createBrowserRouter([
@@ -12,8 +13,12 @@ function App() {
       element: <><Navbar/></>,
     },
     {
-      path: "/home",
+      path: "/home1",
       element: <><Navbar/><Home1/></>,
+    },
+    {
+      path: "/about",
+      element: <><Navbar/><About/></>,
     }
   ])
     {
