b9c8fadbc96d59d8a96cd5969511da92a2f14362
Sujan S <sujanshivananda5@gmail.com>
2025-04-18 19:50:55 +0530

    Add SellWaste component and update routing; modify package.json for dependencies

--- hackfest/src/components/SellWaste.jsx
+++ hackfest/src/components/SellWaste.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import {useForm} from 'react-hook-form';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import { Link } from 'react-router-dom';
 import {useState} from 'react';
 import './SellWaste.css';
 
@@ -147,7 +146,7 @@ return (
     </>
   );
 }
-export default Sellwaste;
+export default sellWaste
     
 
 
