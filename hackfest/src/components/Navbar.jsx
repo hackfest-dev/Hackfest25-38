28f941d124159b3a25afac20356c03b4c2f9bb5c
Sujan S <sujanshivananda5@gmail.com>
2025-04-18 17:53:29 +0530

    background

--- hackfest/src/components/Navbar.jsx
+++ hackfest/src/components/Navbar.jsx
@@ -3,7 +3,21 @@ import { NavLink } from 'react-router-dom'
 
 function Navbar() {
   return (
-    <></>
+    <>
+        <nav>
+            <div className="logo">
+                <h1>Agrigo</h1>
+            </div>
+            <div className="nav-links">
+                <li><NavLink to="/">Home</NavLink></li>
+                <li><NavLink to="/sellwaste">Sell waste</NavLink></li>
+                <li><NavLink to="/about">About</NavLink></li>
+            </div>
+            <div className='profile'>
+                <li><NavLink to="/profile">Profile</NavLink></li>
+            </div>
+        </nav>
+    </>
   )
 }
 
