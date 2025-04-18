289d766ab69abff12d72774d0af6097c1678d2e3
Shodhan Shetty <shodhanshetty700@gmail.com>
2025-04-18 18:43:24 +0530

    home basic

--- hackfest/src/components/Home1.jsx
+++ hackfest/src/components/Home1.jsx
@@ -1,10 +1,33 @@
-import React,{ useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
+import "./Home.css";
 
 
-function Home1() {
-  return (
-    <div>Home1</div>
-  )
-}
+const home= () => {
+    const [location, setLocation] = useState("");
+    const [data, setData] = useState({});
+    const [user, setUser] = useState({
+      username: "",
+      name: "",
+      email: "",
+    });
+    const api_key = `e3c956ab07660d21379180255cb41a28`;
 
-export default Home1
+
+    const userId = localStorage.getItem("userId");
+
+
+    const search = async () => {
+      if (!location) return;
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${api_key}&units=metric`;
+      const res = await fetch(url);
+      const searchData = await res.json();
+      setData(searchData);
+      setLocation("");
+    };
+
+    return (
+      <div classname="welcome" >
+        <h1>HOME</h1>
+      </div>
+    );
+};
